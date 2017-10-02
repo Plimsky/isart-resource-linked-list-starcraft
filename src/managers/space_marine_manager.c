8a8918ef9ce1373691baef8b4b62e253940c2457
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2017-10-02 11:41:29 -0400

    Update spaceMarine manager by adding TODOs

--- src/managers/space_marine_manager.c
+++ src/managers/space_marine_manager.c
@@ -2,25 +2,25 @@
 
 t_spaceMarineArmy *constructorSpaceMarineArmy(int total)
 {
-
+	// TODO
 }
 
 void destructorSpaceMarineArmy(t_spaceMarineArmy *this)
 {
-
+	// TODO
 }
 
 void addSpaceMarine(t_spaceMarineArmy *army, t_spaceMarine *spaceMarine)
 {
-
+	// TODO
 }
 
 void removeSpaceMarine(t_spaceMarineArmy *army, t_spaceMarine *spaceMarine)
 {
-
+	// TODO
 }
 
 bool isSpaceMarineExist(t_spaceMarineArmy *army, t_spaceMarine *spaceMarine)
 {
-
+	// TODO
 }
